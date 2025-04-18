87f603c38fa513219457dee554a8b0bb49fb07a6
Wellington Cimento <wellingtoncimentooficial@gmail.com>
2025-04-17 22:16:42 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+import ffmpeg
+import time
+from datetime import datetime
+import os
+import multiprocessing
+import shutil
+
+BASE_DIR = ""
+TMP_DIR = os.path.join(BASE_DIR, "tmp")
+RTSP_URL = ""  # example rtsp://username:password@ip:port/live/ch00_0
+SEGMENT_TIME = 60  # seconds
+TIMEOUT = 30  # seconds
+IDLE_TIME = TIMEOUT  # seconds
+
+
+def write_log_file(text):
+    with open(os.path.join(BASE_DIR, "log.txt"), "a") as log:
+        log.write(f"{text}\n")
+
+
+def create_tmp_dir():
+    os.makedirs(TMP_DIR, exist_ok=True)
+
+
+def record_stream():
+    while True:
+        try:
+            write_log_file(f"Successfully connected to {RTSP_URL}")
+            (
+                ffmpeg.input(
+                    RTSP_URL, rtsp_transport="tcp", timeout=str(TIMEOUT * 1000000)
+                )
+                .output(
+                    f"{TMP_DIR}/%d-%m-%Y_%H-%M-%S.mp4",
+                    c="copy",
+                    f="segment",
+                    segment_time=SEGMENT_TIME,
+                    reset_timestamps=1,
+                    strftime=1,
+                    **{"c:a": "aac", "b:a": "128k"},
+                )
+                .global_args("-loglevel", "error")
+                .run()
+            )
+            write_log_file("The connection dropped")
+        except ffmpeg.Error as e:
+            write_log_file(
+                f"[{datetime.now()}] FFmpeg failed: {e.stderr.decode() if e.stderr else str(e)}"
+            )
+            write_log_file(f"trying to connect again to {RTSP_URL} in 5 seconds...")
+            time.sleep(5)
+
+
+def is_idle(file_path, idle_seconds):
+    last_modified = os.path.getmtime(file_path)
+    return (time.time() - last_modified) > idle_seconds
+
+
+def organize_records():
+    while True:
+        for filename in os.listdir(TMP_DIR):
+            filepath = os.path.join(TMP_DIR, filename)
+            if not filename.endswith(".mp4"):
+                continue
+            if not os.path.isfile(filepath):
+                continue
+            if not is_idle(filepath, IDLE_TIME):
+                continue
+
+            try:
+                date_str = filename.split("_")[0]
+                target_dir = os.path.join(BASE_DIR, date_str)
+
+                os.makedirs(target_dir, exist_ok=True)
+
+                write_log_file(f"Moving {filename} to {target_dir}")
+                shutil.move(filepath, os.path.join(target_dir, filename))
+
+            except Exception as e:
+                write_log_file(f"Error moving {filename}: {e}")
+
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    create_tmp_dir()
+
+    p1 = multiprocessing.Process(target=record_stream)
+    p2 = multiprocessing.Process(target=organize_records)
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
