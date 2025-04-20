05972cccfbd44160a605eebb2de3ac509a6d30a7
Wellington Cimento <wellingtoncimentooficial@gmail.com>
2025-04-20 13:49:09 -0300

    send emails when fail

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = ""  # example /mnt/hd
 TMP_DIR = os.path.join(BASE_DIR, "tmp")
-SEGMENT_TIME = 60  # seconds
+SEGMENT_TIME = 10  # seconds
 TIMEOUT = 30  # seconds
 IDLE_TIME = TIMEOUT  # seconds
 VIDEO_FORMAT = ".mp4"
@@ -10,3 +10,9 @@ VIDEO_FORMAT = ".mp4"
 camera_list = [
     # example {"name": "Cam1", "url": "rtsp://admin:admin@192.168.0.181:554/live/ch00_0"}
 ]
+
+SMTP_EMAIL_FROM = ""
+SMTP_EMAIL_TO = ""
+SMTP_HOST = ""
+SMTP_PORT = 587
+SMTP_INTERVAL = 1800
