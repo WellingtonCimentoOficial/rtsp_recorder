95f51559229b541a09e13956235ada745fe88a2f
Wellington Cimento <wellingtoncimentooficial@gmail.com>
2025-04-22 04:24:58 -0300

    send email when storage is almost full

--- main.py
+++ main.py
@@ -5,30 +5,20 @@ import os
 import multiprocessing
 import shutil
 from settings import *
-from emails import send_email
+from utils import send_email, create_tmp_dir, Log
 import threading
+from custom_types import Camera
 
 
-def write_log_file(text):
-    with open(os.path.join(BASE_DIR, "log.txt"), "a") as log:
-        now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-        log.write(f"{now} {text}\n")
-
-
-def create_tmp_dir():
-    if os.path.exists(TMP_DIR):
-        shutil.rmtree(TMP_DIR)
-
-    os.makedirs(TMP_DIR, exist_ok=True)
-
-
-def start_ffmpeg(camera):
+def start_ffmpeg(camera: Camera):
     filename = (
         camera["name"].replace(" ", "-").replace("_", "-")
         + "_%d-%m-%Y_%H-%M-%S"
         + VIDEO_FORMAT
     )
     output_path = f"{TMP_DIR}/{filename}"
+
+    Log.write(category=Log.RTSP, message=f"Successfully connected to {camera['url']}")
 
     ffmpeg.input(
         camera["url"], rtsp_transport="tcp", timeout=str(TIMEOUT * 1000000)
@@ -46,47 +36,68 @@ def start_ffmpeg(camera):
     ).run()
 
 
-def send_email_async(subject, body):
-    try:
-        send_email(subject, body)
-        write_log_file("The e-mail was send successfully!")
-    except:
-        write_log_file("The e-mail was not sent!")
+def send_email_async(subject: str, body: str, category: str):
+    threading.Thread(
+        target=send_email,
+        args=(subject, body, category),
+        daemon=True,
+    ).start()
 
 
-def record_stream(camera):
+def storage_checker():
+    done_event = threading.Event()
+
+    while True:
+        storage = shutil.disk_usage(BASE_DIR)
+        total = round(storage.total / (1024**3), 2)
+        used = round(storage.used / (1024**3), 2)
+        free = round(storage.free / (1024**3), 2)
+        percent_used = round((used / total) * 100, 2)
+
+        if percent_used >= 90:
+            text = f"Storage is at {percent_used}% usage."
+            Log.write(category=Log.STORAGE, message=text, level="warning")
+            send_email_async(
+                subject=text,
+                body=f"The storage is at {percent_used}% usage.\nTotal: {total}\nUsed: {used}\nFree {free}",
+                category=Log.STORAGE,
+            )
+            done_event.wait()
+            break
+
+        time.sleep(5)
+
+
+def record_stream(camera: Camera):
     last_email_sent = datetime.now() - timedelta(hours=1)
 
     while True:
         try:
-            write_log_file(f"Successfully connected to {camera['url']}")
             start_ffmpeg(camera)
-            write_log_file("The connection dropped")
         except ffmpeg.Error:
-            write_log_file(
-                f"trying to connect again to {camera['url']} in 5 seconds..."
+            Log.write(
+                category=Log.RTSP,
+                message=f"The connection dropped. Trying to connect again to {camera['url']} in 5 seconds...",
+                level="error",
             )
-            time.sleep(5)
 
         if (datetime.now() - last_email_sent).total_seconds() >= SMTP_INTERVAL:
-            threading.Thread(
-                target=send_email_async,
-                args=(
-                    f"{camera['name']} is down!",
-                    f"The connection to {camera['name']} is down",
-                ),
-                daemon=True,
-            ).start()
-
+            send_email_async(
+                subject=f"{camera['name']} is down!",
+                body=f"The connection to {camera['name']} is down",
+                category=Log.RTSP,
+            )
             last_email_sent = datetime.now()
 
+        time.sleep(5)
 
-def is_idle(file_path, idle_seconds):
+
+def is_idle(file_path: str, idle_seconds: int):
     last_modified = os.path.getmtime(file_path)
     return (time.time() - last_modified) > idle_seconds
 
 
-def replace_metadata(filepath, camera_name, filename, output_path):
+def replace_metadata(filepath: str, camera_name: str, filename: str, output_path: str):
     try:
         title = filename.replace(VIDEO_FORMAT, "")
 
@@ -98,9 +109,15 @@ def replace_metadata(filepath, camera_name, filename, output_path):
 
         os.remove(filepath)
 
-        write_log_file(f"Metadata added to {camera_name} {filename}")
+        Log.write(
+            category=Log.METADATA, message=f"Metadata added to {camera_name} {filename}"
+        )
     except ffmpeg.Error:
-        write_log_file(f"Error replacing metadata {camera_name} {filename}")
+        Log.write(
+            category=Log.METADATA,
+            message=f"Error replacing metadata {camera_name} {filename}",
+            level="error",
+        )
 
 
 def organize_records():
@@ -132,9 +149,16 @@ def organize_records():
                     output_path=os.path.join(camera_dir, new_filename),
                 )
 
-                write_log_file(f"{new_filename} moved to {camera_dir}")
+                Log.write(
+                    category=Log.ORGANIZER,
+                    message=f"{new_filename} moved to {camera_dir}",
+                )
             except Exception as e:
-                write_log_file(f"Error moving {filename}: {e}")
+                Log.write(
+                    category=Log.ORGANIZER,
+                    message=f"Error moving {filename}: {e}",
+                    level="error",
+                )
 
         time.sleep(5)
 
@@ -151,7 +175,12 @@ def run():
 
     organizer = multiprocessing.Process(target=organize_records)
     organizer.start()
+
+    storagechecker = multiprocessing.Process(target=storage_checker)
+    storagechecker.start()
+
     processes.append(organizer)
+    processes.append(storagechecker)
 
     for process in processes:
         process.join()
