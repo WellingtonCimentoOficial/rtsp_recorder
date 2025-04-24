4223575b063d43ed01667317e3ed170708bda6f2
Wellington Cimento <wellingtoncimentooficial@gmail.com>
2025-04-24 13:01:20 -0300

    improving code organization

--- settings.py
+++ settings.py
@@ -2,10 +2,6 @@ import os
 
 BASE_DIR = ""  # example /mnt/hd
 TMP_DIR = os.path.join(BASE_DIR, "tmp")
-SEGMENT_TIME = 10  # seconds
-TIMEOUT = 30  # seconds
-IDLE_TIME = TIMEOUT  # seconds
-VIDEO_FORMAT = ".mp4"
 
 camera_list = [
     # example {"name": "Cam1", "url": "rtsp://admin:admin@192.168.0.181:554/live/ch00_0"}
